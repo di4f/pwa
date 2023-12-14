4873a107cce4b951f4f1fcb6b6a5aaf6983acc76
surdeus <surdeus@gmail.com>
2023-12-14 21:52:17 +0300

    ...

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -4,53 +4,35 @@ import (
 	"github.com/omnipunk/pwa/v9/app"
 	"github.com/omnipunk/pwa/v9/ui"
 	"net/http"
-	"log"
+	//"log"
 )
 
-type Root struct {
+type RootCompo struct {
 	app.Compo
+	children []app.UI
 }
 
-func (r *Root) OnPreRender(c app.Context) {
-	log.Println("prerendering root")
+func Root(children ...app.UI) *RootCompo {
+	ret := &RootCompo{
+		children: children,
+	}
+	return ret
 }
 
-func (r *Root) Render() app.UI {
-	radio := ui.RadioButton()
-	nav := ui.Nav(nil)
-
-	radio.Btn("test1", app.Text("Test1")).
-		Btn("test2", app.Text("Test2")).
-		Btn("test3", app.Text("Test3")).
-		Btn("sub", app.Text("Subs")).
-		OnClick(func(c app.Context, e app.Event){
-			nav.SetValue(c, radio.GetValue(c))
-		})
-
-	nav.Valuer(radio).Default(
-			"test1",
-		).Define(
-			"test1",
-			app.P().Text("Test 1"),
-		).Define(
-			"test2",
-			app.Div().Text("Test 2"),
-		).Define(
-			"test3",
-			app.Strong().Text("Test 3"),
-		).Define(
-			"sub",
-			&RootSub{
-				Parent: nav,
-			},
-		)
-
+func (r *RootCompo) Render() app.UI {
 	return app.Div().ID("root").Body(
 		app.Header().Text("Hello, World!"),
 		app.Nav().Body(
-			radio,
+			app.Button().Text("Subs").OnClick(func(c app.Context, e app.Event){
+				c.Navigate("/test1/")
+			}),
+			app.Button().Text("Root/Hello").OnClick(func(c app.Context, e app.Event){
+				c.Navigate("/")
+			}),
 		),
-		nav,
+		app.Range(r.children).Slice(func(i int) app.UI {
+			return r.children[i]
+		}),
 	)
 }
 
@@ -60,36 +42,18 @@ type RootSub struct {
 }
 
 func (rs *RootSub) Render() app.UI {
-	nav := ui.Nav(rs.Parent)
-	radio := ui.RadioButton()
-
-
-	nav.Valuer(radio).Default(
-			"sub1",
-		).Define(
-			"sub1",
-			app.Text("Sub1"),
-		).Define(
-			"sub2",
-			app.Text("Sub2"),
-		).Define(
-			"sub3",
-			app.Text("Sub3"),
-		)
-
-	radio.Btn("sub1", app.Text("Sub1")).
-		Btn("sub2", app.Text("Sub2")).
-		Btn("sub3", app.Text("Sub3")).
-		OnClick(func(c app.Context, e app.Event){
-			nav.SetValue(c, radio.GetValue(c))
-		})
-
-	return app.Div().Body(
-		app.Nav().Body(
-			radio,
-		),
-		nav,
+	return Root(
+		app.Div().Text("The sub text"),
 	)
+}
+
+type Text struct {
+	app.Compo
+	Text string
+}
+
+func (t *Text) Render() app.UI {
+	return nil
 }
 
 type hello struct {
@@ -100,15 +64,16 @@ func (h *hello) Render() app.UI {
 	ret := app.Div().Body(
 		app.H1().Text("Hello, World!"),
 		app.Button().Text("To main").OnClick(func(c app.Context, e app.Event){
-			c.Navigate("/")
+			c.Navigate("/hello/")
 		}),
 	)
-	return ret
+	return Root(ret)
 }
 
 func main() {
+	app.Route("/", &hello{})
 	app.Route("/hello/", &hello{})
-	app.RouteWithRegexp(".*", &Root{})
+	app.Route("/test1/", &RootSub{})
 	app.RunWhenOnBrowser()
 
 	http.Handle("/", &app.Handler{
